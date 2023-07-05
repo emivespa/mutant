4be92955ab32e4ceb333f8b6707266844fd5204b
Emilio Vesprini <emivespa1@gmail.com>
2023-07-05 14:16:02 +0000

    move from Fargate to EC2

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ resource "aws_ecs_cluster" "mutant" {
 
 # task
 resource "aws_ecs_task_definition" "app_task" {
-  family                   = "mutant-task"
-  container_definitions    = <<DEFINITION
+  family                = "mutant-task"
+  container_definitions = <<DEFINITION
   [
     {
       "name": "mutant-task",
@@ -70,13 +70,14 @@ resource "aws_ecs_task_definition" "app_task" {
           "hostPort": 8080
         }
       ],
-      "memory": 512,
+      "memoryReservation": 512,
       "cpu": 256
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"]
+  # requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
   memory                   = 512
   cpu                      = 256
   execution_role_arn       = aws_iam_role.myEcsTaskExecutionRole.arn
@@ -160,17 +161,18 @@ resource "aws_ecs_service" "app_service" {
   name            = "app-first-service"
   cluster         = aws_ecs_cluster.mutant.id
   task_definition = aws_ecs_task_definition.app_task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
+  # launch_type     = "FARGATE"
+  launch_type   = "EC2"
+  desired_count = 1
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn # Reference the target group
     container_name   = aws_ecs_task_definition.app_task.family
     container_port   = 8080
   }
   network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
-    assign_public_ip = true # Do provide the containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"]
+    subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    # assign_public_ip = true # Do provide the containers with public IPs
+    security_groups = ["${aws_security_group.service_security_group.id}"]
   }
 }
 
